676ca430d70863ca06189d16635123196af449e3
jnowicki <kubavic@interia.pl>
2014-12-19 23:27:07 +0100

    poprawione testy

--- test/models/EventManagerTest.scala
+++ test/models/EventManagerTest.scala
@@ -10,13 +10,13 @@ class EventManagerTest {
 	val eventTwo = Event(Some(2L), new Date(2132121321231L), "Gdańsk")
 	val eventThree = Event(Some(3L), new Date(23123131132123L), "Gdańsk")
 	
-	val presentationOne = Presentation(Some(1L), Some("bbb"), Some("15-16"),Some("Presenter 1"), Some(1L))
-	val presentationTwo = Presentation(Some(2L), Some("ccc"), Some("15-16"),Some("Presenter 1, Presenter 2"), Some(2L))
-	val presentationThree = Presentation(Some(3L), Some("ddd"), Some("16-17"),Some("Presenter 1, Presenter 3"), Some(2L))
+	val presentationOne = Presentation(Some(1L), Some("bbb"), Some("15-16"),Some("Presenter 1"), Some("desc1"), Some(1L))
+	val presentationTwo = Presentation(Some(2L), Some("ccc"), Some("15-16"),Some("Presenter 1, Presenter 2"), Some("desc2"), Some(2L))
+	val presentationThree = Presentation(Some(3L), Some("ddd"), Some("16-17"),Some("Presenter 1, Presenter 3"), Some("desc3"), Some(2L))
   
 	val inputList: List[(Event, Presentation)] = List(
 	    (eventOne, presentationOne), (eventTwo, presentationTwo), (eventTwo, presentationThree),
-	    (eventThree, Presentation(None, None, None, None))
+	    (eventThree, Presentation(None, None, None, None, None))
 	    )
 	
 	val expectedList: List[EventWithPresentations] = List(
